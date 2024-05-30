abbd64725cfcd84bb2b529a9ccab596ac1d392a4
Swapnil Sarwe <swapnilsarwe@gmail.com>
2024-05-30 15:38:39 +0530

    new icons with fill & stroke cleanup

--- src/BladeIoniconsServiceProvider.php
+++ src/BladeIoniconsServiceProvider.php
@@ -1,34 +1,41 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Faisal50x\BladeIonicons;
 
 use BladeUI\Icons\Factory;
+use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\ServiceProvider;
 
-class BladeIoniconsServiceProvider extends ServiceProvider
+final class BladeIoniconsServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     */
-    public function boot()
+    public function register(): void
+    {
+        $this->registerConfig();
+
+        $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
+            $config = $container->make('config')->get('blade-ionicons', []);
+
+            $factory->add('ionicons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
+        });
+    }
+
+    private function registerConfig(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-ionicons.php', 'blade-ionicons');
+    }
+
+    public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__ . '/../resources/svg' => public_path('vendor/blade-ionicons'),
-            ], 'blade-ionicons');
-        }
-    }
+            ], 'blade-si'); // TODO: updating this alias to `blade-ionicons` in next major release
 
-    /**
-     * Register the application services.
-     */
-    public function register()
-    {
-        $this->callAfterResolving(Factory::class, function(Factory $factory){
-            $factory->add('ionicons', [
-                'path' => __DIR__ . '/../resources/svg',
-                'prefix' => 'ionicon',
-            ]);
-        });
+            $this->publishes([
+                __DIR__ . '/../config/blade-ionicons.php' => $this->app->configPath('blade-ionicons.php'),
+            ], 'blade-ionicons-config');
+        }
     }
 }
